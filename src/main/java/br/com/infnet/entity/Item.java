34d7582e782a97c93c3fa5e9db8da72c87e6a286
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2026-03-10 12:40:06 -0300

    refactor: extract item entity from order

---
+++ src/main/java/br/com/infnet/entity/Item.java
@@ -0,0 +1,16 @@
+package br.com.infnet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    private String product;
+    private Double price;
+    private int quantity;
+}
