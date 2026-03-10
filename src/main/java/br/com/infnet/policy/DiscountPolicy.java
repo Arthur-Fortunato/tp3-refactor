5c0cf3c56862157bba253c6f27a01aa54f82e594
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2026-03-10 14:13:30 -0300

    refactor: remove duplicated logic moving discount calc to DiscountPolicy

--- src/main/java/br/com/infnet/policy/DiscountPolicy.java
+++ src/main/java/br/com/infnet/policy/DiscountPolicy.java
@@ -1,7 +1,13 @@
 package br.com.infnet.policy;
 
 public class DiscountPolicy {
-    public double calculateDiscount(double amount, double rate) {
-        return amount * rate;
+    private static final double DISCOUNT_RATE = 0.1;
+
+    public double calculateDiscount(double amount) {
+        return amount * DISCOUNT_RATE;
+    }
+
+    public double calculateFinalTotal(double amount) {
+        return amount - calculateDiscount(amount);
     }
 }
--- src/main/java/br/com/infnet/service/InvoicePrinter.java
+++ src/main/java/br/com/infnet/service/InvoicePrinter.java
@@ -2,19 +2,21 @@ package br.com.infnet.service;
 
 import br.com.infnet.entity.Item;
 import br.com.infnet.entity.Order;
+import br.com.infnet.policy.DiscountPolicy;
 
 public class InvoicePrinter {
-    private OrderCalculator calculator = new OrderCalculator();
+    private DiscountPolicy discountPolicy = new DiscountPolicy();
 
     public void print(Order order) {
         System.out.println("Cliente: " + order.getClient().getClientName());
+
         for (Item item : order.getItems()) {
             System.out.println(item.getQuantity() + "x " + item.getProduct() + " - R$" + item.getPrice());
         }
 
         double total = order.calculateTotal();
-        double discount = calculator.calculateDiscount(total);
-        double finalTotal = calculator.calculateFinalTotal(total);
+        double discount = discountPolicy.calculateDiscount(total);
+        double finalTotal = discountPolicy.calculateFinalTotal(total);
 
         System.out.println("Subtotal: R$" + total);
         System.out.println("Desconto: R$" + discount);
